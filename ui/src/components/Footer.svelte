7a6c041d7c670cb6422ef8698b7e14c101d7076a
James McKenzie <james@jjgmckenzie.ca>
2022-06-30 16:06:36 -0700

    Adds non-existent (yet!) ToS to footer, removed in last commit

--- ui/src/components/Footer.svelte
+++ ui/src/components/Footer.svelte
@@ -1,11 +1,17 @@
 <div class="flex min-w-full fixed bottom-0">
     <footer class="flex-auto text-center text-sm bg-primary-500 text-white">
-        <a href="https://github.com/simulgames/Simul.Games" class="hover:text-blue-700">
+        <a href="https://github.com/simulgames/Simul.Games" class="hover:underline">
             <span class="material-icon">
                 github
             </span>
         </a>
         <br/>
         Copyright © 2022 simul.games.
+        <br/>
+        <span class="text-xs">
+            <a class="inline-block align-baseline hover:underline text-white" href="tos">
+                Terms of Service
+            </a>
+        </span>
     </footer>
 </div>
