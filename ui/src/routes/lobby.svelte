1c150fb3b3623d6430dfcc354d4546f96585bd46
James McKenzie <james@jjgmckenzie.ca>
2022-07-01 10:43:41 -0700

    Creates Lobby page - presently just static, needs wiring.

--- ui/src/routes/lobby.svelte
+++ ui/src/routes/lobby.svelte
@@ -3,85 +3,14 @@
     import Header from "../components/Header.svelte"
     import Footer from "../components/Footer.svelte"
     import WaitingLobby from "../components/WaitingLobby.svelte";
-    import {SendMessage} from "../scripts/WebSocket"
-    import {onMount} from "svelte";
-    import { goto } from "$app/navigation"
-    import {type LobbyData} from "../types/LobbyData"
-    import GameLobby from "../components/GameLobby.svelte";
-    import IconButton from "../components/style/IconButton.svelte";
-
-    function goToHomePage(){
-        goto("/")
-    }
-
-    function gameStarted(){
-        lobbyData["has-started"] = true
-    }
-
-    function goToHomePageIfNotInLobby(e:Event){
-        let userLocation = (e as CustomEvent).detail
-        if(userLocation["inLobby"] == "false"){
-            goToHomePage()
-        }
-    }
-
-    onMount(()=>{
-        SendMessage("UserLocation")
-        SendMessage("GetLobbyDataInternal")
-        document.addEventListener("LobbyLeft",goToHomePage)
-        document.addEventListener("UserLocation",goToHomePageIfNotInLobby)
-        document.addEventListener("GameStarting",gameStarted)
-        document.addEventListener("LobbyData",OnGetLobbyData)
-        document.addEventListener("MemberJoined",OnMemberJoined)
-        document.addEventListener("MemberLeft",OnMemberLeft)
-        return ()=>{
-            document.removeEventListener("LobbyLeft",goToHomePage)
-            document.removeEventListener("GameStarting",gameStarted)
-            document.removeEventListener("UserLocation",goToHomePageIfNotInLobby)
-            document.removeEventListener("LobbyData",OnGetLobbyData)
-            document.removeEventListener("MemberJoined",OnMemberJoined)
-            document.removeEventListener("MemberLeft",OnMemberLeft)
-        }
-    })
-
-
-    let lobbyData : LobbyData = {"client-id": "", "has-started": false,
-        lobby: {id:"","host-id":"",name:""},
-        members: [{id:"",color:"",name:""}]}
-    let loadedData : boolean = false
-
-    function OnGetLobbyData(e:Event){
-        lobbyData = (e as CustomEvent).detail
-        loadedData = true
-    }
-
-    function OnMemberJoined(){
-        SendMessage("GetLobbyDataInternal") // todo - this is lazy, but for feature parity, this is the best approach for now
-    }
-
-
-    function OnMemberLeft(){
-        SendMessage("GetLobbyDataInternal") // todo - this is lazy, but for feature parity, this is the best approach for now
-    }
-    function Exit(){
-        SendMessage("LeaveLobby")
-    }
-
+    let inGame = false
 </script>
 
-<Header>
-    <IconButton icon="exit_to_app" OnClick={Exit}/>
-</Header>
+<Header/>
 
-{#if !lobbyData["has-started"]}
+{#if !inGame}
     <div class="min-w-fit max-w-[95%] w-[50rem] mx-auto">
-        {#if loadedData}
-            <WaitingLobby lobbyData={lobbyData}/>
-        {/if}
+        <WaitingLobby/>
     </div>
     <Footer/>
-{/if}
-
-{#if lobbyData["has-started"]}
-    <GameLobby lobbyData={lobbyData}/>
 {/if}
