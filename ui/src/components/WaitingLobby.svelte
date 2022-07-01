4a5fd87073421853dee4fc70c514e906aad40577
James McKenzie <james@jjgmckenzie.ca>
2022-07-01 10:43:41 -0700

    Creates Lobby page - presently just static, needs wiring.

--- ui/src/components/WaitingLobby.svelte
+++ ui/src/components/WaitingLobby.svelte
@@ -1,28 +1,14 @@
 <script lang="ts">
-    import {onMount} from "svelte";
-    import {type LobbyData} from "../types/LobbyData"
-    import {SendMessage} from "../scripts/WebSocket";
-    export let lobbyData : LobbyData
-
 
     import Card from "./style/Card.svelte";
     import Button from "./style/Button.svelte";
     import Avatar from "./style/Avatar.svelte";
     import LobbyHost from "./style/icons/LobbyHost.svelte";
 
-    let showKey = true
+    let hostName = "James'"
     let gameName = "Word Duel"
+    let lobbyCode = "123456"
 
-    $: keyIcon = showKey ? "key" : "key_off"
-    $: formattedLobbyCode = showKey && lobbyData ? `${lobbyData.lobby.id.substring(0,3)}-${lobbyData.lobby.id.substring(3)}` : "•••-•••"
-
-
-    function leaveLobby(){
-        SendMessage("LeaveLobby")
-    }
-    function startGame(){
-        SendMessage("StartGame")
-    }
 </script>
 
 <style>
@@ -33,37 +19,33 @@
 
 <div class="select-none">
     <Card>
-        <h1 class="text-xl text-primary-700 font-bold font-serif">{lobbyData.lobby.name}</h1>
+        <h1 class="text-xl text-primary-700 font-bold font-serif">{hostName} Lobby</h1>
         <div class="flex justify-between">
-            <button on:click={()=>{showKey=!showKey}}>
-                <span class="material-icons text-sm text-primary-700">{keyIcon}</span>
-                <span class="font-mono text-sm">{formattedLobbyCode}</span>
-            </button>
+            <div>
+                <span class="material-icons text-sm text-primary-700">
+                    key
+                </span>
+                <span class="font-mono text-sm">{lobbyCode}</span>
+            </div>
             <span class="text-primary-700 font-serif text-lg">
                 Playing <i>{gameName}</i>
             </span>
         </div>
         <hr class="mb-5">
-        <ul class="inline-grid justify-between w-full grid-columns list-none">
-                {#each lobbyData.members as member}
-                    <li class="text-center">
-                    <Avatar fill={member.color}>
-                        {#if member.id === lobbyData.lobby["host-id"]}
-                            <LobbyHost fill="#3B82F6"/>
-                        {/if}
-                    </Avatar>
-                    <span class="text-lg font-mono text-primary-700 {member.id === lobbyData['client-id'] ? 'font-bold' : ''}">{member.name}</span>
-                    </li>
-                {/each}
+        <ul class="inline-grid justify-between w-full grid-columns">
+            <li>
+                <Avatar>
+                    <LobbyHost fill="#3B82F6"/>
+                </Avatar>
+            </li>
         </ul>
         <hr class="mt-5">
         <div class="flex justify-between p-3">
             <div class="flex-[0.25] w-full">
-                <Button text="LEAVE" icon="logout" OnClick={leaveLobby}/>
+                <Button text="LEAVE" icon="logout"/>
             </div>
             <div class="flex-[0.25] w-full">
-                <!-- todo - change to a "READY" button for non hosts -->
-                <Button text="START" icon="play_arrow" disabled={lobbyData['client-id']!==lobbyData.lobby["host-id"]} OnClick={startGame}/>
+                <Button text="START" icon="play_arrow"/>
             </div>
         </div>
     </Card>
--- ui/src/components/style/Avatar.svelte
+++ ui/src/components/style/Avatar.svelte
@@ -1,10 +1,10 @@
 <script>
     import DefaultAvatar from "./icons/DefaultAvatar.svelte";
-    export let fill = "grey"
+    export let colour = "pink"
 </script>
 
 <div class="w-full select-none relative">
-    <DefaultAvatar {fill}/>
+    <DefaultAvatar fill={colour}/>
     <span class="absolute bottom-1.5 right-1.5 w-[25%]">
         <slot/>
     </span>
