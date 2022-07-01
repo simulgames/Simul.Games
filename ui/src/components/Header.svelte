cd6ac1e892c3dbd03400ff2fff7085e77d2dc755
James McKenzie <james@jjgmckenzie.ca>
2022-07-01 10:57:23 -0700

    select=none to header

--- ui/src/components/Header.svelte
+++ ui/src/components/Header.svelte
@@ -1,3 +1,3 @@
-<div class="py-2 mb-2 font-mono font-bold italic text-xl px-10 shadow-lg bg-primary-800">
+<div class="py-2 mb-2 font-mono font-bold italic text-xl px-10 shadow-lg bg-primary-800 select-none">
     <h5 class="text-primary-50">simul.games</h5>
 </div>
