6ae31d193a2f88407b55bb2ae463eb6f2cdbbcd1
James McKenzie <james@jjgmckenzie.ca>
2022-07-02 14:32:51 -0700

    cleanup redundant code + improves mobile ui

--- ui/src/components/style/Tile.svelte
+++ ui/src/components/style/Tile.svelte
@@ -1,16 +1,10 @@
 <script>
-    import { onMount} from "svelte";
-
     export let character = ""
     export let style = ""
-    let show = false
-    onMount(() => {
-        show = true
-    })
 </script>
 
 <div class="select-none w-full bg-white text-primary-600 shadow-md rounded mx-auto min-w-[2rem] max-w-[10vh] aspect-square flex-grow {style} relative">
-    <span class="absolute bottom-0 right-0 top-0 left-0 font-bold font-serif text-center text-3xl m-auto h-[min-content]">
+    <span class="absolute bottom-0 right-0 top-0 left-0 font-bold font-serif text-center text-2xl md:text-3xl m-auto h-[min-content]">
         {character}
     </span>
 </div>
