4b02503c58f70b650b5c5cab8d68d302eb56b4a7
James McKenzie <james@jjgmckenzie.ca>
2022-07-01 18:06:00 -0700

    colour results, not dynamically and no "pizazz" yet

--- ui/src/components/subcomponents/GameBoard.svelte
+++ ui/src/components/subcomponents/GameBoard.svelte
@@ -8,12 +8,32 @@
 
     $: getCharacter = (turn : string,character : string) => {
         let currentTurn = Guesses.length-1
-        if(turn < currentTurn){
+        if(Guesses[turn] != null){
             return Guesses[turn][character]
         }
         if(turn == currentTurn){
             if(character < CurrentWord.length){
                 return CurrentWord[character]
+            }
+        }
+        return ""
+    }
+
+    $: getStyle = (turn : string,character : string) => {
+        if(Results[turn] != null){
+            let result = Results[turn][character]
+            if(Results[turn][character] != null){
+                switch (result){
+                    case 0:{
+                        return "tile-with-result no-letter"
+                    }
+                    case 1:{
+                        return "tile-with-result has-letter"
+                    }
+                    case 2:{
+                        return "tile-with-result has-letter-at-space"
+                    }
+                }
             }
         }
         return ""
@@ -30,7 +50,7 @@
     <div class="grid-column mx-auto grid flex-grow gap-[1vw]">
         {#each Array(height) as _,turn}
             {#each Array(width) as _,character}
-                <Tile character={getCharacter(turn,character)}/>
+                <Tile character={getCharacter(turn,character)} style={getStyle(turn,character)}/>
             {/each}
         {/each}
     </div>
