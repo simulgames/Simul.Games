aa48413df0821557804c45679e5f32ec579c8a17
James McKenzie <james@jjgmckenzie.ca>
2022-07-01 18:34:26 -0700

    exit button in lobby header

--- ui/src/components/Header.svelte
+++ ui/src/components/Header.svelte
@@ -1,3 +1,4 @@
-<div class="py-2 mb-2 font-mono font-bold italic text-xl px-10 shadow-lg bg-primary-800">
+<div class="py-2 mb-2 font-mono font-bold italic text-xl px-10 shadow-lg bg-primary-800 flex justify-between">
     <h5 class="text-primary-50">simul.games</h5>
+    <slot/>
 </div>
---
+++ ui/src/components/style/IconButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let icon : string = ''
+    export let OnClick : (event : Event) => void = null
+</script>
+
+<button class="text-white material-icons" on:click={OnClick}>
+    {icon}
+</button>
