37b9e1122788aec6500eaf1281abdadd890e3800
James McKenzie <james@jjgmckenzie.ca>
2022-07-02 15:02:16 -0700

    refactors getStyle & frees game board from unnecessary dependencies

--- ui/src/components/subcomponents/GameBoard.svelte
+++ ui/src/components/subcomponents/GameBoard.svelte
@@ -1,44 +1,6 @@
 <script lang="ts">
     import Tile from "../style/Tile.svelte";
-    export let Guesses : [number[string]] = []
-    export let Results : [number[number]] = []
-    export let CurrentWord : string
-    let width = 5
-    let height = 6
-
-    $: getCharacter = (turn : string,character : string) => {
-        let currentTurn = Guesses.length
-        if(turn != height && turn == currentTurn){
-            if(character < CurrentWord.length){
-                return CurrentWord[character]
-            }
-            return ""
-        }
-        if(Guesses[turn] != null){
-            return Guesses[turn][character]
-        }
-        return ""
-    }
-
-    $: getStyle = (turn : string,character : string) => {
-        if(Results[turn] != null){
-            let result = Results[turn][character]
-            if(Results[turn][character] != null){
-                switch (result){
-                    case 0:{
-                        return "tile-with-result no-letter"
-                    }
-                    case 1:{
-                        return "tile-with-result has-letter"
-                    }
-                    case 2:{
-                        return "tile-with-result has-letter-at-space"
-                    }
-                }
-            }
-        }
-        return ""
-    }
+    export let TileBoard : [[{letter:string,style:string}]]
 </script>
 
 <style>
@@ -47,12 +9,10 @@
     }
 </style>
 
-<div class="flex mx-[30vw]">
-    <div class="grid-column mx-auto grid flex-grow gap-[1vw]">
-        {#each Array(height) as _,turn}
-            {#each Array(width) as _,character}
-                <Tile character={getCharacter(turn,character)} style={getStyle(turn,character)}/>
-            {/each}
+<div class="grid-column mx-auto grid flex-grow gap-[1vw]">
+    {#each TileBoard as turn}
+        {#each turn as tile}
+            <Tile character={tile.letter} style={tile.style}/>
         {/each}
-    </div>
+    {/each}
 </div>
