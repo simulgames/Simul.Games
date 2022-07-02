776ad7a6dce574ee816a2b29e8c9ec27d9406fb0
James McKenzie <james@jjgmckenzie.ca>
2022-07-01 18:06:00 -0700

    colour results, not dynamically and no "pizazz" yet

--- ui/src/components/style/Tile.svelte
+++ ui/src/components/style/Tile.svelte
@@ -1,7 +1,49 @@
 <script>
+    import {afterUpdate, onMount} from "svelte";
+
     export let character = ""
     export let style = ""
+    let show = false
+    onMount(() => {
+        show = true
+    })
 </script>
-<div class="w-full bg-white shadow-md rounded mx-auto min-w-[2rem] aspect-square flex-grow {style}">
-    {character}
+
+<style>
+
+    @keyframes flip {
+        from {
+            transform: rotateX(180deg);
+        }
+    }
+    @keyframes result-color {
+        to{
+            color: var(--color-to-transition-to);
+            background-color: var(--bg-color-to-transition-to);
+        }
+    }
+
+    .tile-with-result{
+        animation: result-color 0.4s forwards steps(1), flip 0.45s;
+        --color-to-transition-to: white;
+    }
+
+    .no-letter{
+        --bg-color-to-transition-to: #787c7e;
+    }
+
+    .has-letter {
+        --bg-color-to-transition-to: #c9b458;
+    }
+
+    .has-letter-at-space {
+        --bg-color-to-transition-to: #6aaa64;
+    }
+
+</style>
+
+<div class="select-none w-full bg-white text-primary-600 shadow-md rounded mx-auto min-w-[2rem] aspect-square flex-grow {style} relative">
+    <span class="absolute bottom-0 right-0 top-0 left-0 font-bold font-serif text-center text-3xl m-auto h-[min-content]">
+        {character}
+    </span>
 </div>
--- ui/src/components/subcomponents/Keyboard.svelte
+++ ui/src/components/subcomponents/Keyboard.svelte
@@ -2,39 +2,41 @@
     import KeyButton from "../style/KeyButton.svelte";
     export let isEnterDisabled = false
 </script>
+<div class="select-none text-center">
 
-<div class="text-center mt-4">
-    <KeyButton key="q"/>
-    <KeyButton key="w"/>
-    <KeyButton key="e"/>
-    <KeyButton key="r"/>
-    <KeyButton key="t"/>
-    <KeyButton key="y"/>
-    <KeyButton key="u"/>
-    <KeyButton key="i"/>
-    <KeyButton key="o"/>
-    <KeyButton key="p"/>
-</div>
-<div class="text-center mt-1">
-    <KeyButton key="a"/>
-    <KeyButton key="s"/>
-    <KeyButton key="d"/>
-    <KeyButton key="f"/>
-    <KeyButton key="g"/>
-    <KeyButton key="h"/>
-    <KeyButton key="j"/>
-    <KeyButton key="k"/>
-    <KeyButton key="l"/>
-    <KeyButton icon="backspace" key="Backspace"/>
-</div>
-<div class="text-center mt-1">
-    <div class="w-4"></div>
-    <KeyButton key="z"/>
-    <KeyButton key="x"/>
-    <KeyButton key="c"/>
-    <KeyButton key="v"/>
-    <KeyButton key="b"/>
-    <KeyButton key="n"/>
-    <KeyButton key="m"/>
-    <KeyButton icon="keyboard_return" key="Enter" width="w-20" disabled={isEnterDisabled}/>
+    <div class="mt-4">
+        <KeyButton key="q"/>
+        <KeyButton key="w"/>
+        <KeyButton key="e"/>
+        <KeyButton key="r"/>
+        <KeyButton key="t"/>
+        <KeyButton key="y"/>
+        <KeyButton key="u"/>
+        <KeyButton key="i"/>
+        <KeyButton key="o"/>
+        <KeyButton key="p"/>
+    </div>
+    <div class="mt-1">
+        <KeyButton key="a"/>
+        <KeyButton key="s"/>
+        <KeyButton key="d"/>
+        <KeyButton key="f"/>
+        <KeyButton key="g"/>
+        <KeyButton key="h"/>
+        <KeyButton key="j"/>
+        <KeyButton key="k"/>
+        <KeyButton key="l"/>
+        <KeyButton icon="backspace" key="Backspace"/>
+    </div>
+    <div class="mt-1">
+        <div class="w-4"></div>
+        <KeyButton key="z"/>
+        <KeyButton key="x"/>
+        <KeyButton key="c"/>
+        <KeyButton key="v"/>
+        <KeyButton key="b"/>
+        <KeyButton key="n"/>
+        <KeyButton key="m"/>
+        <KeyButton icon="keyboard_return" key="Enter" width="w-20" disabled={isEnterDisabled}/>
+    </div>
 </div>
