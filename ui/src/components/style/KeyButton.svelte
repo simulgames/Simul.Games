4d47f2bb32d76f4d1980039501016db3007dba6e
James McKenzie <james@jjgmckenzie.ca>
2022-07-02 13:10:33 -0700

    keyboard tweaks

--- ui/src/components/style/KeyButton.svelte
+++ ui/src/components/style/KeyButton.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     export let key = ''
     export let icon: string = ''
-    export let width : string = 'w-12'
     function pressKey(){
         let keyboardEvent = new KeyboardEvent('keydown',{
             key: key
@@ -11,11 +10,16 @@
     export let disabled = false
 </script>
 
-<button class=" border-slate-300 border-2 h-12 {width} bg-white shadow-md rounded mx-auto flex-grow text-3xl align-text-bottom
-        hover:bg-slate-100 hover:border-slate-500
-        {icon !== '' ? 'material-icons' : ''}"
+<button class=" border-slate-300 border-2 bg-white shadow-md rounded w-full flex-grow
+        hover:bg-slate-100 hover:border-slate-500 relative h-16"
         on:click={pressKey}
         {disabled}
     >
-    {icon !== '' ? icon : key.toUpperCase()}
+    <span class="{icon !== '' ? 'material-icons' : 'font-bold'}
+    absolute bottom-0 right-0 left-0 top-0 m-auto text-center h-[min-content]
+    text-xl
+    sm:text-2xl
+    lg:text-3xl">
+        {icon !== '' ? icon : key.toUpperCase()}
+    </span>
 </button>
