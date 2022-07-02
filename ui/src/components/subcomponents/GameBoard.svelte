d87d300056cbf2bb4077a1e2ad30a506ed492e22
James McKenzie <james@jjgmckenzie.ca>
2022-07-01 17:01:18 -0700

    now accepts inputs, places letters on the game board correctly and sends to the server if requested - need to parse server response for colour.

--- ui/src/components/subcomponents/GameBoard.svelte
+++ ui/src/components/subcomponents/GameBoard.svelte
@@ -1,8 +1,23 @@
-<script>
+<script lang="ts">
     import Tile from "../style/Tile.svelte";
-
+    export let Guesses : [number[string]] = []
+    export let Results : [number[number]] = []
+    export let CurrentWord : string
     let width = 5
     let height = 6
+
+    $: getCharacter = (turn : string,character : string) => {
+        let currentTurn = Guesses.length-1
+        if(turn < currentTurn){
+            return Guesses[turn][character]
+        }
+        if(turn == currentTurn){
+            if(character < CurrentWord.length){
+                return CurrentWord[character]
+            }
+        }
+        return ""
+    }
 </script>
 
 <style>
@@ -13,9 +28,9 @@
 
 <div class="flex mx-[30vw]">
     <div class="grid-column mx-auto grid flex-grow gap-[1vw]">
-        {#each Array(width) as x}
-            {#each Array(height) as y}
-                <Tile/>
+        {#each Array(height) as _,turn}
+            {#each Array(width) as _,character}
+                <Tile character={getCharacter(turn,character)}/>
             {/each}
         {/each}
     </div>
