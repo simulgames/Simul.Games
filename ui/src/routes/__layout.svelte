aa8c7e977542dd61211d6b1db52f5a8007d53b50
James McKenzie <james@jjgmckenzie.ca>
2022-07-02 20:14:16 -0700

    dark mode xor override

--- ui/src/routes/__layout.svelte
+++ ui/src/routes/__layout.svelte
@@ -1,11 +1,17 @@
-<script lang="ts">
-    $: darkMode = () : string => {
+<svelte:head>
+    <script>
+        let override = ('override-browser-dark-mode' in localStorage)
+        let defaultMode = (window.matchMedia('(prefers-color-scheme: dark)').matches)
+        if(override ^ defaultMode){
+            document.documentElement.classList.add('dark')
+        }
+        else {
+            document.documentElement.classList.remove('dark')
+        }
+    </script>
+</svelte:head>
 
-        return 'dark'
-    }
-</script>
-
-<div class="{darkMode()}">
+<div>
     <div class="absolute h-full w-full -z-10 top-0 bg-primary-100 dark:bg-zinc-900"></div>
     <slot/>
 </div>
