38c0fe5c445e3d7ac393d00c7376857ca5985e38
James McKenzie <james@jjgmckenzie.ca>
2022-07-05 19:40:28 -0700

    styles enter as disabled while keeping button enabled, providing visual feedback for game if wrong input used also fixes missing 'g' in keyboard also adds default toast duration

--- ui/src/components/style/KeyButton.svelte
+++ ui/src/components/style/KeyButton.svelte
@@ -12,20 +12,12 @@
 </script>
 
 <button class="h-[3rem] border-2 bg-white rounded flex-grow
-        enabled:hover:bg-slate-100 enabled:hover:border-slate-500 relative
-        enabled:shadow-md
-        enabled:border-slate-300
-        disabled:bg-gray-400
-        disabled:text-white
-        dark:bg-zinc-700
+        relative
         dark:border-zinc-800
-        dark:enabled:hover:bg-zinc-600
         dark:text-white
-        dark:disabled:bg-zinc-800
-        dark:disabled:text-zinc-400
+        {disabled ? 'dark:bg-zinc-800 dark:text-zinc-400 bg-gray-400 text-white' : 'bg-zinc-700 dark:hover:bg-zinc-600 border-slate-300 shadow-md hover:bg-slate-100 hover:border-slate-500'}
         {Class}"
         on:click={pressKey}
-        {disabled}
     >
     <span class="{icon !== '' ? 'material-icons' : 'font-bold'}
     absolute bottom-0 right-0 left-0 top-0 m-auto text-center h-[min-content]
