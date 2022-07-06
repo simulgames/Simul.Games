fe09efa1f55591f09832275b053d1577050c530a
James McKenzie <james@jjgmckenzie.ca>
2022-07-06 12:04:10 -0700

    adds header redirect to main page

--- ui/src/components/Header.svelte
+++ ui/src/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="py-2 mb-2 font-mono font-bold italic text-xl px-10 shadow-lg bg-primary-800 flex justify-between select-none">
-    <h5 class="text-primary-50">simul.games</h5>
+    <a href="/"><h5 class="text-primary-50">simul.games</h5></a>
     <div>
         <DarkModeIcon/>
         <slot/>
