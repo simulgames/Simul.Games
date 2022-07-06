c454af3d53888ac3eee7f0effa90bccb08397aed
James McKenzie <james@jjgmckenzie.ca>
2022-07-06 12:44:35 -0700

    tweak: row style only added when necessary

--- ui/src/components/subcomponents/GameBoard.svelte
+++ ui/src/components/subcomponents/GameBoard.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import Tile from "../style/Tile.svelte";
     export let Class : string = ""
-    export let TileBoard : [{row:[{letter:string,style:string}],rowStyle: { class:string,delay:number }}]
+    export let TileBoard : [{row:[{letter:string,style:string}],rowStyle?: { class:string,delay:number }}]
     export let TileClass : string = ""
 </script>
 
@@ -14,9 +14,13 @@
 <div class="grid-column grid {Class}">
     {#each TileBoard as turn}
         {#each turn.row as tile, i}
-            <div class={turn.rowStyle.class} style={`animation-delay: ${turn.rowStyle.delay*i}ms`}>
+            {#if turn.rowStyle !== null}
+                <div class={turn.rowStyle.class} style={`animation-delay: ${turn.rowStyle.delay*i}ms`}>
+                    <Tile character={tile.letter} Class={tile.style + " " + TileClass}/>
+                </div>
+            {:else}
                 <Tile character={tile.letter} Class={tile.style + " " + TileClass}/>
-            </div>
+            {/if}
         {/each}
     {/each}
 </div>
