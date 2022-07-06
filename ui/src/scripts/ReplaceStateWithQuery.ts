1a455488818cf5d41457fa045b2553d854ef30cf
James McKenzie <james@jjgmckenzie.ca>
2022-07-06 11:54:10 -0700

    adds lobby?id=ID

---
+++ ui/src/scripts/ReplaceStateWithQuery.ts
@@ -0,0 +1,13 @@
+// credit : https://dev.to/mohamadharith/mutating-query-params-in-sveltekit-without-page-reloads-or-navigations-2i2b
+
+export const replaceStateWithQuery = (values: Record<string, string>) => {
+    const url = new URL(window.location.toString());
+    for (let [k, v] of Object.entries(values)) {
+        if (!!v) {
+            url.searchParams.set(encodeURIComponent(k), encodeURIComponent(v));
+        } else {
+            url.searchParams.delete(k);
+        }
+    }
+    history.replaceState({}, '', url);
+};
