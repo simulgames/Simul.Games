e3d101c4722aceb124a5ab04deacf785c09dbc6a
James McKenzie <james@jjgmckenzie.ca>
2022-07-06 14:21:24 -0700

    add start new game footer.

---
+++ ui/src/components/subcomponents/NextGameFooter.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import {scale} from 'svelte/transition'
+    import Button from "../style/Button.svelte";
+    import {SendMessage} from "../../scripts/WebSocket.js";
+    export let isHost : boolean = false
+</script>
+
+<div transition:scale class="absolute bottom-0 bg-blue-600 flex shadow-lg w-full text-white font-bold text-2xl text-center">
+    {#if isHost}
+        <Button icon="forward" text="New Game" OnClick={()=>{SendMessage('NewGame')}}/>
+    {:else}
+        <span class="w-full">Waiting for Host to Start New Game</span>
+    {/if}
+</div>
