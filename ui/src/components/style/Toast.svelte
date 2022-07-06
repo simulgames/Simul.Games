2f313d119ee01cbcea02fd26a37f0251e7032b64
James McKenzie <james@jjgmckenzie.ca>
2022-07-05 18:02:05 -0700

    adds "toasts" & toast container, using svelte transitions & stores

---
+++ ui/src/components/style/Toast.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import {fade,scale} from "svelte/transition";
+    import Paper from "./Paper.svelte";
+    export let text = ''
+</script>
+
+<div class="m-2 shadow-xl" in:scale out:fade>
+    <Paper>
+        <span class="p-3 text-2xl">{text}</span>
+    </Paper>
+</div>
