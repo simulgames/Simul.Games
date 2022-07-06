307f1f974f8216e20519fe9dda544b87318a52f6
James McKenzie <james@jjgmckenzie.ca>
2022-07-05 17:59:21 -0700

    paper & card split

--- ui/src/components/style/Card.svelte
+++ ui/src/components/style/Card.svelte
@@ -1,3 +1,7 @@
-<div class="w-full bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-primary-700 dark:bg-zinc-800 dark:text-white">
+<script lang="ts">
+    import Paper from "./Paper.svelte";
+</script>
+
+<Paper Class="px-8 pt-6 pb-8 mb-4">
     <slot/>
-</div>
+</Paper>
