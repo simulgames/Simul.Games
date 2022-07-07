d1db500e6a95205601b82df93daa0ff734f25384
James McKenzie <james@jjgmckenzie.ca>
2022-07-06 18:10:29 -0700

    correctly imports css + tweaks

--- ui/src/routes/lobby.svelte
+++ ui/src/routes/lobby.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import "../app.css";
     import Header from "../components/Header.svelte"
     import Footer from "../components/Footer.svelte"
     import WaitingLobby from "../components/WaitingLobby.svelte";
