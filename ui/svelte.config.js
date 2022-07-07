3eb3d74c250d44b640e8faca6f7c4bbd686ed1a7
James McKenzie <james@jjgmckenzie.ca>
2022-07-06 17:02:31 -0700

    adds cloudflare deploy

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ platform: 'node' }),
 
 		vite: {
 			test: {
