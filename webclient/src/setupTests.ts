67ee576d9241f8019409c7010597a4dd20c10e74
James McKenzie <james@jjgmckenzie.ca>
2022-07-08 12:26:18 -0700

    fixes LobbyCodeInput.test.ts

--- webclient/src/setupTests.ts
+++ webclient/src/setupTests.ts
@@ -1,7 +1,10 @@
 import "@testing-library/jest-dom/extend-expect"
 import { act } from "@testing-library/svelte"
 
-export async function MockSendMessage(header: string, body?: any) {
+export async function MockMessageReceivedFromServer(
+    header: string,
+    body?: any
+) {
     let invalidLobbyEvent = new CustomEvent(header, {
         bubbles: true,
         cancelable: false,
