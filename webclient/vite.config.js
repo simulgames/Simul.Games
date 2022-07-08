5bef82c656cb1bec8bea47a72811fed48199beb6
James McKenzie <james@jjgmckenzie.ca>
2022-07-08 12:45:37 -0700

    collects coverage in json file

--- webclient/vite.config.js
+++ webclient/vite.config.js
@@ -12,6 +12,9 @@ export default {
         environment: "jsdom",
         // in-source testing
         includeSource: ["src/**/*.{js,ts,svelte}"],
+        coverage:{
+            reporter:["json"]
+        },
         deps: {
             // Put Svelte component here, e.g., inline: [/svelte-multiselect/, /msw/]
             inline: [],
